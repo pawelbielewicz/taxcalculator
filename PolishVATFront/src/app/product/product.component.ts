75efff216a33138065babc6bbd1c9e27cf8d8806
pbielewicz <pawel.bielewicz@soprasteria.com>
2020-05-08 12:19:25 +0200

    Add form to front end

--- PolishVATFront/src/app/product/product.component.ts
+++ PolishVATFront/src/app/product/product.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { ProductService } from './product.service';
 import { Product } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-product',
@@ -17,15 +17,18 @@ export class ProductComponent implements OnInit {
   allVats: Observable<string[]>;
   productIdUpdate = null;
   massage = null;
+  product: Product;
+  products: Array<Product> = [];
 
   constructor(private formbulider: FormBuilder, private productService: ProductService) { }
 
   ngOnInit() {
-    this.productForm = this.formbulider.group({
-      Name: ['', [Validators.required]],
-      Price: ['', [Validators.required]],
-      Quantity: ['', [Validators.required]],
-      VatName: ['', [Validators.required]]
+    this.product = new Product();
+    this.productForm = new FormGroup({
+      prodName: new FormControl(''),
+      prodPrice: new FormControl(''),
+      prodQuantity: new FormControl(''),
+      prodVat: new FormControl(''),
     });
     this.loadAllVats();
   }
@@ -34,5 +37,32 @@ export class ProductComponent implements OnInit {
     this.allVats = this.productService.getAllVats();
   }
 
+  onFormSubmit() {
+    this.dataSaved = false;
+    this.product.Name = this.productForm.value.prodName;
+    this.product.Price = this.productForm.value.prodPrice;
+    this.product.Quantity = this.productForm.value.prodQuantity;
+    this.product.VatType = this.productForm.value.prodVat;
+
+    this.products.push(this.product);
+    this.CalculateProducts(this.products);
+    this.productForm.reset();
+  }
+
+  CalculateProducts(products: Product[]) {
+    this.productService.calculateGrossPrice(products).subscribe(
+      () => {
+        this.dataSaved = true;
+        this.massage = 'Record saved Successfully';
+        this.productForm.reset();
+      }
+    )
+  }
+
+  resetForm() {
+    this.productForm.reset();
+    this.massage = null;
+    this.dataSaved = false;
+  }
 
 }
